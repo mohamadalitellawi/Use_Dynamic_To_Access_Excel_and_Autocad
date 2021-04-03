9a94d68cd826a2cfe54e6121505d3ff997865554
Ali <mohamadalitellawi@gmail.com>
2021-04-03 19:13:01 +0300

    Add project files.

---
+++ autocad_dynamic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autocad_dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic autocadType = Type.GetTypeFromProgID("AutoCAD.Application");
+            var AcadApp = Activator.CreateInstance(autocadType);
+            
+            AcadApp.Visible = true;
+
+            double[] CenterOfCircle = new double[3];
+            CenterOfCircle[0] = 0;
+            CenterOfCircle[1] = 0;
+            CenterOfCircle[2] = 0;
+
+            double RadiusOfCircle = 100;
+
+            var Circle = AcadApp.ActiveDocument.ModelSpace.AddCircle(CenterOfCircle, RadiusOfCircle);
+            Circle.color = 3;
+
+            AcadApp.ZoomExtents();
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ excel_dynamic/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel_dynamic
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static List<Person> persons = new List<Person>();
+        static Program()
+        {
+            persons.Add(new Person("Frank", 25));
+            persons.Add(new Person("Joe", 24));
+        }
+
+        static void Main(string[] args)
+        {
+            dynamic excelType = Type.GetTypeFromProgID("Excel.Application");
+            var excelObj = Activator.CreateInstance(excelType);
+            excelObj.Visible = true;
+
+            excelObj.Workbooks.Add();
+            dynamic workSheet = excelObj.ActiveSheet;
+
+            workSheet.Cells[1, 1] = "Names";
+            workSheet.Cells[1, 2] = "Age";
+
+            int rowIndex = 1;
+            foreach (var person in persons)
+            {
+                rowIndex++;
+                workSheet.Cells[rowIndex, 1] = person.Name;
+                workSheet.Cells[rowIndex, 2] = person.Age;
+            }
+        }
+    }
+}
